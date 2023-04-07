b9ef0c038683e92b1f9e5de090184cda01002a23
Vengeance <130109175+VengeanceFromHallOfLockers@users.noreply.github.com>
2023-04-06 20:54:55 -0400

    Create old lock sets.lua

---
+++ old lock sets.lua
@@ -0,0 +1,16 @@
+0.115 (flash hb)
+0.109 (astro dot or any dot)
+0.132 (astro dot or any dot)
+
+0.115 (flash hb)
+0.115 (astro dot or any dot)
+0.155 (astro dot or any dot)
+
+0.115 (flash hb)
+0.115 (astro dot or any dot)
+
+0.115 (flash hb)
+0.155 (astro dot or any dot)
+
+0.115 (flash hb)
+0.165 (astro dot or any dot) 
