9c8f1f2968ecc80e7f8a745f781d4a428b10bd88
Vengeance <130109175+VengeanceFromHallOfLockers@users.noreply.github.com>
2023-04-06 20:55:52 -0400

    Create recent lock sets.lua

---
+++ recent lock sets.lua
@@ -0,0 +1,6 @@
+0.13419 (2-3 taps) (LowerTorso)
+0.13237 (2-3 taps) (LowerTorso)
+0.13481 (2-3 taps) (LowerTorso)
+0.13745 (2-3 taps) (LowerTorso)
+0.13189 (2-3 taps) (LowerTorso)
+0.13913 (2-3 taps) (LowerTorso)
